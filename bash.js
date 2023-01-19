f3de8e24bcb750fdf3481c7ae8e20651e5bc038b
mandy-chin <chin.mandyy@gmail.com>
2023-01-19 16:48:33 -0500

    ls.js

--- bash.js
+++ bash.js
@@ -3,4 +3,4 @@ const pwd = require('./pwd');
 const ls = require('./ls');
 
 // if a user enters 'pwd' as an argument, we can then call it
-pwd();
+ls();
--- ls.js
+++ ls.js
@@ -2,15 +2,20 @@ module.exports = function () {
   process.stdout.write("prompt >");
   const fs = require("fs");
 
-  process.stdin.on("data", () => {
-    // `files` will be an array of filenames, like ['bash.js', 'pwd.js']
-    fs.readdir("./", "utf8", (err, files) => {
-      if (err) {
-        throw err;
-      } else {
-        process.stdout.write(files.join("\n"));
-        process.stdout.write("\nprompt > ");
-      }
-    });
+  process.stdin.on("data", (data) => {
+    const input = data.toString().trim();
+
+    if (input === "ls") {
+      fs.readdir("./", "utf8", (err, files) => {
+        if (err) {
+          throw err;
+        } else {
+          process.stdout.write(files.join("\n"));
+          process.stdout.write("\nprompt > ");
+        }
+      });
+    } else {
+      process.stdout.write("prompt > ");
+    }
   });
 };
